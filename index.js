a33875a1edda0376b6d219b31467e293d7e2f31d
darinetag <tagdarinefatma@gmail.com>
2023-08-08 12:57:47 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+console.log(moo)
+console.log(neigh)
+console.log(baa)
+console.log(oink)
+console.log(cluck)
+const [bessie,  , dolly, babe, little] = farmAnimals.split(' ')
+console.log(bessie)
+console.log()
+console.log(dolly)
+console.log(babe)
+console.log(little)
+const [blackAndWhite,  , black, pink,  ] = farmAnimals.split(' ')
+console.log(blackAndWhite)
+console.log()
+console.log(black)
+console.log(pink)
+console.log()
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+console.log(colors)
+const [r, o, y, g, b, i, v] = colors
+console.log(colors)
+const[, , , , , indg, ] = colors
+console.log(colors)
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +33,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName, color, song, job, partner} = muppet
+console.log(muppet)
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +50,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { song2, song4} = nestedMuppet.album.theMuppetMovie
+const { nestedJob, nestedPartner} = nestedMuppet
 
 // Strings
 
